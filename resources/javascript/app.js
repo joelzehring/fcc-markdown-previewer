633855ced13a518220630b9a2676b11e4b37f900
Joel Zehring <joelzehring@gmail.com>
2019-11-28 22:02:12 -0700

    Changed url in example image

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -33,7 +33,7 @@ Markdown is great for writing about code:
 
 You can learn more about me at [my site](https://joelz.me)!
 
-![Portrait of Joel](https://lh3.googleusercontent.com/rjcP8un4RKFcOdcSUpZ65UTtI-S_Q_EkszQsbUzchrf2FWt9eYZQmycxrN-gzvqGVQLrkyxlurzIy6PnvViKnbMfc2XBbiHDebL29GFmzwpCWoOhhL5Z2z0e3vZ4jkZbwNQRwJASQKUi__EWHbgyaOkjxhF0Z-CeOdqQesdo1LA1TvMtwa12KHWVbuK9vx8249WYOtPo7nEQmtdNLxufT0-d0mhG5PbZQoY7I-rU5bLzuUWDUfhMC1riT9P0LHoKCgG7FX1E079-vlt2eUT3af5XbYthayFcoip5vFQOa2JJl72Q1I79WDtc-tswBabv3rdVQy6OoISDMFwH47UdRKSkNDVUwpHOQxsIJAqy8Qcozovqrj8Xno4RaH1iYZ9jTVXnELACerTmi8nrAXmzmfY3LXhU6QJTu6Vfa7F5H_T0zL9Mhug76WN6QlVxj-ax9686IRnez26x9Lnl8ubUaGlup9ZrYAD0qa-yfi7MkqeRjzKvcKMENpIm6qe8kWK7622DUV1WEP1fSNJi3Jv-efiuLkkLx2TCzCUg2VeHK57tcOGHhpf8WLwpaz7JKsSjaD5twG_lAIGkxknwKs01RHHPejwTLVnC_6rXfR10tiVByf3X4ojJhBxmei7JVQHX5szKbjbjYSfXh42IJZvdLnlQnz7iHFsBYLbICMpZSuwrxPSEeuFm9ox304UZT4aEb1ZoCuwtJDu54Q-qilU76oc8rn3M7ae6zpvShohm-q1huHepSw)
+![Markdown logo](https://en.wikipedia.org/wiki/Markdown#/media/File:Markdown-mark.svg)
 `
 
 	let previewText = marked( editorText, {gfm: true, breaks: true} );
